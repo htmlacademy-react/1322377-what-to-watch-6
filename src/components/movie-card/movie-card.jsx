ae69396e8310c73e3ea7f24a5dbbe92da600866b
Artem Cherenkov <art.cherenkov@gmail.com>
2021-02-20 16:35:06 +0700

    Добавлен стейт-менеджмент для активной карточки фильма WIP: линтер ругается на неиспользуемую переменную

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import movieProp from '../../types/movie.prop';
 
-const MovieCard = ({film}) => {
-  const {name, posterImage} = film;
+const MovieCard = ({film, handleMovieCardMouseOver, handleMovieCardMouseLeave}) => {
+  const {id, name, posterImage} = film;
 
   return (
-    <article className="small-movie-card catalog__movies-card">
+    <article
+      className="small-movie-card catalog__movies-card"
+      onMouseOver={handleMovieCardMouseOver(id)}
+      onMouseLeave={handleMovieCardMouseLeave}
+    >
       <div className="small-movie-card__image">
         <img src={posterImage} alt={name} width={280} height={175} />
       </div>
@@ -19,6 +24,8 @@ const MovieCard = ({film}) => {
 
 MovieCard.propTypes = {
   film: movieProp.isRequired,
+  handleMovieCardMouseOver: PropTypes.func.isRequired,
+  handleMovieCardMouseLeave: PropTypes.func.isRequired,
 };
 
 export default MovieCard;
--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {nanoid} from "nanoid";
 
@@ -6,10 +6,20 @@ import MovieCard from "../movie-card/movie-card";
 import movieProp from "../../types/movie.prop";
 
 const MoviesList = ({movies}) => {
+  const [activeMovie, setActiveMovie] = useState(null);
+
+  const handleMovieCardMouseOver = (movieId) => () => setActiveMovie(movieId);
+  const handleMovieCardMouseLeave = () => setActiveMovie(null);
+
   return (
     <div className="catalog__movies-list">
       {movies.map((movie) => (
-        <MovieCard key={nanoid()} film={movie} />
+        <MovieCard
+          key={nanoid()}
+          film={movie}
+          handleMovieCardMouseOver={handleMovieCardMouseOver}
+          handleMovieCardMouseLeave={handleMovieCardMouseLeave}
+        />
       ))}
     </div>
   );
