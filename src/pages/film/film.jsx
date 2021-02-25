9534e4d41a0dc3eb83f2784421ae42fca87ebba5
Artem Cherenkov <art.cherenkov@gmail.com>
2021-02-25 14:28:19 +0700

    Добавлены компоненты табов и компонент списка табов

--- src/pages/film/film.jsx
+++ src/pages/film/film.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import moment from "moment";
 
 import Footer from "../../components/footer/footer";
 import MoviesList from "../../components/movies-list/movies-list";
 import Logo from "../../components/logo/logo";
+import MovieTabs from "../../components/movie-tabs/movie-tabs";
 import movieProp from '../../types/movie.prop';
+
+import MovieTabOverview from "../../components/movie-tab-overview/movie-tab-overview";
+import MovieTabDetails from "../../components/movie-tab-details/movie-tab-details";
+import MovieTabReviews from "../../components/movie-tab-reviews/movie-tab-reviews";
+
+export const MovieTab = {
+  OVERVIEW: `Overview`,
+  DETAILS: `Details`,
+  REVIEWS: `Reviews`,
+};
 
 const FilmPage = ({movie, sameMovies}) => {
   const {posterImage, name, genre, releaseDate} = movie;
+
+  const [movieTab, setMovieTab] = useState(MovieTab.OVERVIEW);
+  const handleMovieTabChange = (newTab) => setMovieTab(newTab);
+
   return (
     <>
       <section className="movie-card movie-card--full">
@@ -56,33 +71,10 @@ const FilmPage = ({movie, sameMovies}) => {
               <img src={posterImage} alt={name} width={218} height={327} />
             </div>
             <div className="movie-card__desc">
-              <nav className="movie-nav movie-card__nav">
-                <ul className="movie-nav__list">
-                  <li className="movie-nav__item movie-nav__item--active">
-                    <a href="#" className="movie-nav__link">Overview</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Details</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
-              <div className="movie-rating">
-                <div className="movie-rating__score">8,9</div>
-                <p className="movie-rating__meta">
-                  <span className="movie-rating__level">Very good</span>
-                  <span className="movie-rating__count">240 ratings</span>
-                </p>
-              </div>
-              <div className="movie-card__text">
-                <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave`&apos;s friend and protege.</p>
-                <p>Gustave prides himself on providing first-class service to the hotel`&apos;s guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave`&apos;s lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
-                <p className="movie-card__director"><strong>Director: Wes Andreson</strong></p>
-                <p className="movie-card__starring">
-                  <strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other</strong></p>
-              </div>
+              <MovieTabs activeTab={movieTab} onChange={handleMovieTabChange} />
+              {movieTab === MovieTab.OVERVIEW && <MovieTabOverview />}
+              {movieTab === MovieTab.DETAILS && <MovieTabDetails />}
+              {movieTab === MovieTab.REVIEWS && <MovieTabReviews />}
             </div>
           </div>
         </div>
